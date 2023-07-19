263ef126bcfbd5b19c38573b7c80160fceb124a9
hossam43 <hossamayman876.gmail.com>
2023-07-19 17:22:20 +0300

    add json data

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const PORT = 5000;
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
